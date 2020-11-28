9470041adf114ccc2950a29bd6d7a66bef1455a7
aksHITa47 <73430168+aksHITa47@users.noreply.github.com>
2020-11-28 21:50:26 +0530

    Update perceval.py

--- task-8/perceval.py
+++ task-8/perceval.py
@@ -3,23 +3,15 @@ from perceval.backends.core.git import Git
 from github import Github
 from pprint import pprint
 import json
+import os
 # Github username
 username = input("Enter the organization name:")
 # pygithub object
-g = Github()
-data = []
+g = Github("access_token")
 # get that user by username
 user= g.get_user(username)
 for repo in user.get_repos():
-    print(repo)
-    repo_url = 'https://github.com/amfoss/repo.git'#URL of one repo at a time 
-    repo_dir = '/tmp/cms.git' #directory to clone the repo
-    git= Git(uri=repo_url,gitpath=repo_dir)
-    for commit in git.fetch():
-        data.append(commit)
-      # pyob=json.loads(commit)
-with open("commits.json", "w") as write_file:
-        json.dump(data, write_file)
-       
-
-       
+    repos=repo.name
+    print(repos)
+    os.system("perceval git --json-line https://github.com/amfoss/%s >> commits.json"%repos)
+    
