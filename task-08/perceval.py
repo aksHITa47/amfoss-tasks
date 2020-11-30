d5a6e94848c247fcc3e52b38fe34a841d64c9d8e
aksHITa47 <73430168+aksHITa47@users.noreply.github.com>
2020-11-30 10:07:25 +0530

    Update perceval.py

--- task-08/perceval.py
+++ task-08/perceval.py
@@ -6,7 +6,7 @@ username = input("Enter the organization name:")
 # pygithub object
 g = Github("access_token")
 # get that org by username
-user= g.get_user(username)
+user = g.get_user(username)
 for repo in user.get_repos():
     repos=repo.name
     print(repos)
