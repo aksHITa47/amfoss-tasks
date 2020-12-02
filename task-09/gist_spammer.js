c54997896336847d9d65e1bc50c30eff5b8e8940
aksHITa47 <73430168+aksHITa47@users.noreply.github.com>
2020-12-02 23:11:48 +0530

    Update gist_spammer.js
    Its Id with a d not a D!!

--- task-09/gist_spammer.js
+++ task-09/gist_spammer.js
@@ -3,7 +3,7 @@ var interval = 1000  ; // in milliseconds
 var count = 10 ; //number of times to send
 var i = 0 ;
 var timer = setInterval(function(){
-        document.getElementByID("new_comment_field").innerHTML = message;
+        document.getElementById("new_comment_field").innerHTML = message;
         document.querySelector('#partial-new-comment-form-actions>.btn').click();
 	i++;
 	if(i==count)
